54c132af97542f959b6beaace6b64d596e6401e4
gutocheida <guto.cheida@yahoo.com.br>
2024-05-03 19:22:10 -0300

    Adicionando query SQL do teste
    Adicionando query SQL do teste

---
+++ comissao_vendedores.sql
@@ -0,0 +1,22 @@
+with comissoes_vendedor AS (
+    select
+        vendedor,
+        sum(valor) AS total_recebido,
+        row_number() over (partition by vendedor order by "data") AS ordem_transferencia
+    from
+        tabela_comissoes
+    group by
+        vendedor, "data"
+)
+select distinct
+    vendedor
+from
+    comissoes_vendedor
+where
+    ordem_transferencia <= 3
+group by
+    vendedor
+having
+    sum(total_recebido) >= 1024
+order by
+    vendedor ASC;
